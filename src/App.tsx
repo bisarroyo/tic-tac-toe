d4e3fb1612476d7f5478885183af16d9fd9e6db3
Bismark Arroyo <bis.arroyo@gmail.com>
2023-01-25 21:06:24 -0600

    Created first component

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,46 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const TURNS = {
+  x: 'x',
+  o: 'o'
+}
 
+const WINNER_COMBS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+  [0, 3, 6],
+  [2, 5, 8]
+]
+
+
+
+
+function App() {
+  const [board , setBoard] = useState(() => Array(9).fill(null))
+
+  const updateBoard = () => {
+
+  }
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <main className='container'>
+      <h1>TIC-TAC-TOE</h1>
+      <section className='board'>
+        {board.map((_, i) => {
+          return (
+            <div className='square' key={i}>
+              <div className='item' onClick={()=>updateBoard}>{i}</div>
+            </div>
+          )
+        })}
+      </section>
+      <section className='buttom'>
+
+      </section>
+    </main>
   )
 }
 
